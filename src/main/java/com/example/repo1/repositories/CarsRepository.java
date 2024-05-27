66cd6e7b3043de90cb585f0c233d7b3216b75935
Calyen93 <genco.claudio3@gmail.com>
2024-05-27 11:03:10 +0200

    Add Repo

--- src/main/java/com/example/repo1/repositories/CarsRepository.java
+++ src/main/java/com/example/repo1/repositories/CarsRepository.java
@@ -2,6 +2,8 @@ package com.example.repo1.repositories;
 
 import com.example.repo1.models.Cars;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarsRepository extends JpaRepository<Cars, Long> {
 }
